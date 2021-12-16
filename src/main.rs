0e0e09d8af3f6d5fc29dcb8f97b727c9b8402be7
Igor L. Vicente <igorlvicente@protonmail.com>
2021-12-16 17:30:09 -0300

    Development until page 54

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls(); // Clears the window
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Dragon")
+        .build()?;
+
+    main_loop(context, State::new())
 }
